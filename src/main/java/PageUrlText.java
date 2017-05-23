9e7d23bd3b60190dc8111f6e5cf19ece6bc4577f
sutos88 <sutos88@gmail.com>
2017-05-23 19:42:09 +0200

    work in progress #2

---
+++ src/main/java/PageUrlText.java
@@ -0,0 +1,18 @@
+
+public class PageUrlText {
+    String url;
+    String text;
+
+    public PageUrlText(String url, String text) {
+        this.url = url;
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
--- src/main/java/SitemapGenerator.java
+++ src/main/java/SitemapGenerator.java
@@ -24,222 +24,92 @@ public class SitemapGenerator {
 
     public static void main(String[] args) throws InterruptedException {
         init();
-        String actualSite="http://users.atw.hu/de-mi/";
-
+//        String actualSite="http://alaszkakft.hu/";
+//        String actualSite="http://users.atw.hu/de-mi/";
+        String actualSite="http://nagev.hu";
+//        String actualSite="http://cinemacity.hu";
         driver.get(actualSite);
 
-        int depth = 5;
+        int depth = 2;
         int depthCounter = 0;
 
-        List<WebElement> links = driver.findElements(By.tagName("a"));
+        mapItems.add("HOME\n");
 
-        siteMapGen(depth, links, depthCounter, actualSite);
+        siteMapGen(depth, depthCounter++, actualSite, actualSite);
 
-//        String pageSource = driver.getPageSource();
-//        System.out.println(pageSource);
-
-//        String actualSite="http://users.atw.hu/de-mi/";
-//        String actualSite="https://gamestar.hu/";
-//        String actualSite="https://it.inf.unideb.hu/honlap/prog1/";
-//        String actualSite="http://www.nagev.hu/";
-
-//        String actualSite="https://mobilarena.hu/";
-//        int border=2;
-//        int depth=border;
-//
-//        driver.get(actualSite);
-//
-//        siteMapGen(depth, actualSite, border);
-
-//            2.version
-//        List<WebElement> links = driver.findElements(By.tagName("a"));
-//
-//        for (WebElement link : links) {
-//            if (link.getAttribute("href").startsWith(actualSite)) {
-//                siteMapGen(depth, link.getText(), border, link.getAttribute("href"));
-//                break;
-//            }
-//        }
-//        2. version end
-
+        System.out.println("############ R E S U L T ##############");
         for (String item : mapItems) {
             System.out.print(item);
         }
 
-//        for (int i = 0; i < 3; i++) {
-//            driver.get(actualSite);
-//
-//            List<WebElement> links = driver.findElements(By.tagName("a"));
-//            for (WebElement link : links) {
-//
-//                if (link.getAttribute("href") != null) {
-//                    if (link.getAttribute("href").toString().startsWith("https://444.hu")) {
-//                        System.out.println("***");
-//                        System.out.print(link.getAttribute("href"));
-//                        System.out.println("---");
-//                    }
-//                }
-//            }
-//        }
-
     }
-//    public static void siteMapGen (int depth, String actualSite, int border) {
-//        WebDriver driver = new ChromeDriver();
-//        driver.get(actualSite);
-//
-//        List<WebElement> links = driver.findElements(By.tagName("a"));
-//
-//        for (WebElement link : links) {
-//            if (link.getAttribute("href") != null) {
-//                String linkUrl = link.getAttribute("href").toString();
-//                if (linkUrl.startsWith(actualSite) && !mapitems.contains(linkUrl)) {
-//                    for (int i = 0; i < border; i++) {
-//                        mapitems.add("\t");
-//                    }
-//                    mapitems.add(linkUrl);
-//                    border++;
-//                    depth--;
-//                    actualSite = linkUrl;
-//                    siteMapGen(depth, actualSite, border);
-//                }
-//            }
-//        }
-//
-//    }
 
-//    public static void siteMapGen (int depth, String actualSite, int border) throws InterruptedException {
-//        if (depth == 0) {
-//            driver.navigate().back();
-//            return;
-//        }
-//        border-=depth;
-//        String linkUrl = "";
-//        List<WebElement> links = driver.findElements(By.tagName("a"));
-//        int db = 0;
-//        for (WebElement link : links) {
-//            if (link.getAttribute("href") != null) {
-//                System.out.println(link.getText());
-//                if (link.getAttribute("href").toString().startsWith(actualSite) && link.isDisplayed() && link.getAttribute("href").toString().equals(actualSite) && !mapitems.contains(link.getText())) {
-//                    for (int i = 0; i < border; i++) {
-//                        mapitems.add("\t");
-//                    }
-//                    mapitems.add(link.getText());
-//                    mapitems.add("\n");
-//                    db++;
-//                }
-//            }
-//        }
-//        if (db==0) {
-//            return;
-//        }
-//        for (String item : mapitems) {
-//            System.out.print(item);
-//        }
-//        for (WebElement link : links) {
-//            if (link.getAttribute("href") != null) {
-//                linkUrl = link.getAttribute("href");
-//                if (linkUrl.startsWith(actualSite) &&  link.isDisplayed() && !linkUrl.equals(actualSite) && !mapitems.contains(link.getText())) {
-//                    System.out.println(actualSite);
-//                    System.out.println(linkUrl);
-//                    driver.findElement(By.linkText(link.getText())).click();
-//                    System.out.println("****");
-//                    siteMapGen(depth--, linkUrl, border++);
-//                    driver.navigate().back();
-//                }
-//            }
-//        }
-//        driver.navigate().back();
-//    }
 
-//    public static void siteMapGen (int depth, String actualSiteText, int border, String actualLink) {
-//        if (depth == 0) {
-//            driver.navigate().back();
-//            return;
-//        }
-//
-//        border-=depth;
-//
-////        driver.findElement(By.linkText(actualSiteText)).click();
-//
-//        List<WebElement> previousLinks = driver.findElements(By.tagName("a"));
-//        for (WebElement link : previousLinks) {
-//            if (link.getText().equalsIgnoreCase(actualSiteText)) {
-//                System.out.println(link.getText());
-//                link.click();
-//                System.out.println("Klikk a metódusban");
-//                break;
-//            }
-//        }
-//
-//
-//        List<WebElement> links = driver.findElements(By.tagName("a"));
-//        List<String> actualList = new ArrayList<String>();
-//
-//        for (WebElement link : links) {
-//            if (link.getAttribute("href") != null && link.getAttribute("href").startsWith(actualLink) && link.getText()!=actualSiteText && !mapitems.contains(link.getText())) {
-//                actualList.add(link.getText());
-//            }
-//        }
-//
-//        if (actualList.size()==0) {
-//            driver.navigate().back();
-//            return;
-//        }
-//
-//        for (String acItem : actualList) {
-//            for (int i = 0; i < border; i++) {
-//                System.out.println(acItem);
-//                mapitems.add("\t");
-//                mapitems.add(acItem + "\n");
-//                siteMapGen(depth--, acItem, border++, driver.getCurrentUrl());
-//                driver.navigate().back();
-//            }
-//        }
-//    }
-
-    public static void siteMapGen(int depth, List<WebElement> actualWebElements, int depthCounter, String actualSite) {
-        if (depthCounter == depth) {
+    public static void siteMapGen(int depth, int depthCounter, String actualSite, String homeUrl) {
+        if (depthCounter >= depth) {
+            System.out.println("### counter>=depth###");
+            depthCounter--;
             return;
         }
+        List<WebElement> actualWebElements = driver.findElements(By.tagName("a"));
+
+        if (actualWebElements.size()==0) {
+            depthCounter--;
+            return;
+        }
+        List<String> actMapItems = new ArrayList<String>();
+        List<String> actMapItemUrls = new ArrayList<String>();
+        List<PageUrlText> actMapItemsUrl = new ArrayList<PageUrlText>();
 
         for (WebElement element : actualWebElements) {
-            if (element.getAttribute("href") != null && !element.getAttribute("href").equals("") && !element.getAttribute("href").equals("#")
-                    && !mapItems.contains(element.getText()) && !mapItemsUrl.contains(element.getAttribute("href")) /*&& element.getAttribute("href").startsWith(actualSite)*/) {
+            if (element.getAttribute("href") != null && element.getText() != null && !element.getText().equals("") &&
+                    !element.getAttribute("href").equals("#") && !mapItems.contains(element.getText()) &&
+                    !mapItemsUrl.contains(element.getAttribute("href")) && element.isDisplayed() &&
+                    element.getAttribute("href").startsWith(homeUrl) /*&&
+                    !element.getAttribute("href").equals(driver.getCurrentUrl())*/) {
 
                 System.out.println(element.getText());  //for test.
 
-                mapItemsUrl.add(element.getAttribute("href"));
-
                 for (int j = 0; j < depthCounter; j++) {
-                    mapItems.add("\t");
+                    actMapItems.add("\t");
                 }
 
-                mapItems.add(element.getText().toString() + "\n");
-
-                driver.findElement(By.linkText(element.getText())).click();
-
-                try {
-                    Thread.sleep(1000);
-                    System.out.println("Waiting to load page...");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                List<WebElement> nextLinks = driver.findElements(By.tagName("a"));
-
-                siteMapGen(depth, nextLinks, depthCounter++, driver.getCurrentUrl());
-
-                mapItems.add("depthCounter:" + depthCounter + "\n"); //for test.
-
-                driver.navigate().back();
-
-                try {
-                    Thread.sleep(1000);
-                    System.out.println("Waiting to load previous page...");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                actMapItems.add(element.getText().toString() + "\n");
+                actMapItemUrls.add(element.getAttribute("href"));
+                actMapItemsUrl.add(new PageUrlText(element.getAttribute("href"), element.getText()));
             }
         }
+
+        mapItems.addAll(actMapItems);
+        mapItemsUrl.addAll(actMapItemUrls);
+        if (actMapItemsUrl.size()==0) {
+            depthCounter--;
+            return;
+        }
+        for (PageUrlText element : actMapItemsUrl) {
+
+            driver.findElement(By.linkText(element.getText())).click();
+
+            try {
+                Thread.sleep(1000);
+                System.out.println("Waiting to load page...");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            siteMapGen(depth, depthCounter+1, driver.getCurrentUrl(), homeUrl);
+
+            System.out.println("depthCounter:" + depthCounter + "\n"); //for test.
+
+            driver.navigate().back();
+
+            try {
+                Thread.sleep(1000);
+                System.out.println("Waiting to load previous page...");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        depthCounter--;
     }
 }
